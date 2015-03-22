621fc0bdcd9e7c86cad0546401519b8add46d3da
Darkchicken <joshshucker@hotmail.com>
2015-03-22 14:57:24 -0700

    Fixed white flash at end of cutscene

--- Assets/Scripts/CutsceneCode.cs
+++ Assets/Scripts/CutsceneCode.cs
@@ -22,11 +22,16 @@ public class CutsceneCode : MonoBehaviour {
 	}
 	void Update(){
 
-			if(Input.GetMouseButtonDown(0)){
-			//if (Time.time >= nextImageTime  ) {             
-				currentImage++;             
-				//nextImageTime += imageTime;     
+		if(Input.GetMouseButtonDown(0)){
+		//if (Time.time >= nextImageTime  ) { 
+			if(currentImage==CutsceneImage.Length-1){
+				Application.LoadLevel(nextScene.name);
 			}
+			else{
+				currentImage++;
+			}
+			//nextImageTime += imageTime;     
+		}
 
 	}
 	void OnGUI() {
@@ -37,17 +42,17 @@ public class CutsceneCode : MonoBehaviour {
 		//}
 		//While currentImage points to a valid location, check the time.  If it is time for the next image, change
 		//to the next image.  In either case, display the currnt image.
-		if (currentImage < CutsceneImage.Length) {
+		//if (currentImage < CutsceneImage.Length) {
 			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), CutsceneImage [currentImage]); 
 			//if(Input.GetMouseButtonDown(0)){
 			//if (Time.time >= nextImageTime  ) {             
 			//	currentImage++;             
 				//nextImageTime += imageTime;     
 			//}
-		} 
+		//} 
 		//When all images are done, go to level select.
-		else {
-			Application.LoadLevel(nextScene.name);
-		}
+		//else {
+			//Application.LoadLevel(nextScene.name);
+		//}
 	}
 }
