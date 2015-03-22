05cb2d3e1aa6c8685412cbcc068f0d0087e76401
Darkchicken <joshshucker@hotmail.com>
2015-03-22 13:51:40 -0700

    Updating

--- Assets/Scripts/StageSelect.cs
+++ Assets/Scripts/StageSelect.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class StageSelect : MonoBehaviour {
 
+	public void OnClickLevel1()
+	{
+		//Application.LoadLevel("BasicLevel");
+		Application.LoadLevel("Level1Cutscene");
+	}
 	public void OnClickLevel5()
 	{
 		//Application.LoadLevel("BasicLevel");
