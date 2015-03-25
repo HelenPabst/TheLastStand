24d9b89bd7150175d2d2a14f72f9bfeb5cb75493
Joel Lee <lee.joel395@gmail.com>
2015-03-25 14:44:15 -0700

    Fading Arrow prototype
    Very broken code at various stages of functionality.

--- Assets/Scripts/BasicProjectile.cs
+++ Assets/Scripts/BasicProjectile.cs
@@ -6,7 +6,7 @@ public class BasicProjectile : MonoBehaviour
     private double timeSpawned;
     private double selfDestructTime;
     private double currentTime;
-
+	private GameObject StuckProjectile;
 	// Use this for initialization
 	void Start () 
     {
@@ -29,9 +29,16 @@ public class BasicProjectile : MonoBehaviour
 		this.gameObject.tag = "";
 		ObjectPool.instance.PoolObject(this.gameObject);
     }
+
 	public void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.tag.Equals ("Obstacle"))
+		if (other.tag.Equals ("Obstacle")){
+			Debug.Log ("Killing myself");
+			StuckProjectile = ObjectPool.instance.GetObjectForType("StuckProjectile", true);
+			StuckProjectile.transform.position = transform.position;
+			StuckProjectile.transform.rotation = transform.rotation;
+				//Instantiate(GameObject.FindGameObjectWithTag("StuckArrow"), this.transform.position, this.transform.rotation);
 			RemoveArrow ();
+		}
 	}
 }
---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Fade: MonoBehaviour{
+	private float timeUntilFade = 10.0F;
+	private float fadeLerpConstant = 0.1F;
+	private float clearThreshold = 10.0F;
+	
+	int state = 0;
+	float counter = 0F;
+	
+	void Awake()
+	{
+		counter = timeUntilFade;
+	}	
+	
+	void Update()
+	{
+		switch (state)
+		{
+		case 0:
+			counter -= Time.deltaTime;
+			if (counter < 0F){
+				state++;
+				Debug.Log ("FADING");
+			}
+			break;
+		case 1:
+			renderer.material.color = Color.Lerp(renderer.material.color, Color.clear, fadeLerpConstant);
+			Debug.Log ("HELLO");
+			if(this.renderer.material.color.a < clearThreshold){
+				RemoveArrow ();
+			}
+			break;
+		}
+	}
+	public void RemoveArrow()
+	{
+		Debug.Log("Elvis has left the building");
+		this.gameObject.tag = "";
+		ObjectPool.instance.PoolObject(this.gameObject);
+
+	}
+}
