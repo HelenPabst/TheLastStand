47a4d7aa181ad9feebba5239dfdee3a8dadcb241
Reiden <reideng_flip@yahoo.com>
2015-04-02 02:16:11 -0700

    Changed cutscenes from GUI textures to sprites
    This change was made in prepearation for adding fade-to-black
    functionality.

--- Assets/Scripts/CutsceneCode.cs
+++ Assets/Scripts/CutsceneCode.cs
@@ -1,13 +1,15 @@
 ﻿//notes to self: 
-//Maybe do fade ins and outs.
-//The white screen is back.  Will doing fadeouts cover it up?
+//Do fade ins and outs.
+//Resize doesn't work right
 
 using UnityEngine;
 using System.Collections;
 
 public class CutsceneCode : MonoBehaviour {
-	//An array of plain images
-	public Texture[] cutsceneImage;
+	//An array of plain images, old code
+	//public Texture[] cutsceneImage;
+	//An array of cutscene images
+	public Sprite[] cutsceneSprites;
 	//An array of time durations corresponding to CutsceneImage
 	public float[] cutsceneTime;
 	//Holds the time for the next still image to trigger
@@ -16,6 +18,15 @@ public class CutsceneCode : MonoBehaviour {
 	private int currentImage = 0;
 	//Next scene to load
 	public Object nextScene;
+	//Reference to the fader script, maybe unneccesary
+	//public FaderCode fadeCode;
+	//Tells if needs to fade
+	private bool startFade = false;
+
+	//Timer
+	private float timer = 0.0f;
+	//
+
 
 	/* Old code for click-based progression
 	//Responds to user clicks to update scene.
@@ -32,11 +43,44 @@ public class CutsceneCode : MonoBehaviour {
 		}
 	}*/
 
-	//Sets nextFrameTime to allow the first cutscene image.
+	//Sets initial values to allow the first cutscene image.
 	void Start(){
+		//Resize();
 		nextFrameTime = cutsceneTime[0];
+		GetComponent<SpriteRenderer>().sprite = cutsceneSprites[0];
+
 	}
 
+	//
+	void Update(){
+		//succesful no-fading code
+		timer += Time.deltaTime;
+		if(currentImage < cutsceneSprites.Length){
+			if(timer >= nextFrameTime){
+				//needFade = true;
+				currentImage++;
+				nextFrameTime += cutsceneTime[currentImage];
+				GetComponent<SpriteRenderer>().sprite = cutsceneSprites[currentImage];
+			}
+		}
+		else{
+			Application.LoadLevel(nextScene.name);
+		}
+	}
+
+	//Code to resize the image
+	void Resize()
+	{
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		
+		float worldScreenHeight = Camera.main.orthographicSize * 2;
+		float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+		transform.localScale = new Vector3(
+			worldScreenWidth / sr.sprite.bounds.size.x,
+			worldScreenHeight / sr.sprite.bounds.size.y, 1);
+	}
+
+	/*old gui code
 	//Keeps the current display updated to the correct image.
 	void OnGUI() {
 		if (Time.time >= nextFrameTime) {
@@ -49,5 +93,5 @@ public class CutsceneCode : MonoBehaviour {
 			}
 		}
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), cutsceneImage [currentImage]);
-	}
+	}*/
 }
---
+++ Assets/Scripts/FaderCode.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaderCode : MonoBehaviour {
+	//
+	public bool leaveFade = true;
+	//Reference to main cutscene code
+	public CutsceneCode sceneCode;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if(){
+
+		}
+		else{
+
+		}*/
+	}
+
+	void LeaveFade(){
+
+	}
+}
