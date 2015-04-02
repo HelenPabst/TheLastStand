77202e0ba73f11c67e15e4d58588793ec5888b94
Reiden <reideng_flip@yahoo.com>
2015-04-02 04:12:05 -0700

    Cutscenes now fade in/out
    Cutscenes now fade in and out of black between stills.   Also removed
    old Canvases.  Two issues remain.  The resize function doesn't work,
    though the default looks workable for now.  Also, you must still
    manually set the two arrays to the same size.  Not sure how to make them
    automatically the same.

--- Assets/Scripts/CutsceneCode.cs
+++ Assets/Scripts/CutsceneCode.cs
@@ -1,6 +1,6 @@
 ﻿//notes to self: 
-//Do fade ins and outs.
-//Resize doesn't work right
+//Still have to manually make the two arrays the same size in inspector.  Is there a way to fix this?
+//Resize function doesn't work right
 
 using UnityEngine;
 using System.Collections;
@@ -21,11 +21,13 @@ public class CutsceneCode : MonoBehaviour {
 	//Reference to the fader script, maybe unneccesary
 	//public FaderCode fadeCode;
 	//Tells if needs to fade
-	private bool startFade = false;
-
+	private bool doFade = false;
+	//Tells if needs to come back from fade
+	private bool doUnfade = false;
+	//Speed at which the image fades in and out
+	private float fadeSpeed = 2.0f;
 	//Timer
 	private float timer = 0.0f;
-	//
 
 
 	/* Old code for click-based progression
@@ -48,11 +50,27 @@ public class CutsceneCode : MonoBehaviour {
 		//Resize();
 		nextFrameTime = cutsceneTime[0];
 		GetComponent<SpriteRenderer>().sprite = cutsceneSprites[0];
-
+		GetComponent<SpriteRenderer>().color = Color.clear;//start 'faded'
+		doUnfade = true;//fade to image from start
 	}
 
 	//
 	void Update(){
+		//if fading in or out, the timer does not increment
+		if(doFade){
+			FadetoBlack();
+		}
+		else if(doUnfade){
+			FadetoImage();
+		}
+		else{
+			timer += Time.deltaTime;
+			if(timer >= nextFrameTime){
+				doFade = true;
+			}
+		}
+
+		/*
 		//succesful no-fading code
 		timer += Time.deltaTime;
 		if(currentImage < cutsceneSprites.Length){
@@ -65,10 +83,10 @@ public class CutsceneCode : MonoBehaviour {
 		}
 		else{
 			Application.LoadLevel(nextScene.name);
-		}
+		}*/
 	}
 
-	//Code to resize the image
+	//Code to resize the image.  DOES NOT CURRENTLY WORK.
 	void Resize()
 	{
 		SpriteRenderer sr = GetComponent<SpriteRenderer>();
@@ -78,6 +96,35 @@ public class CutsceneCode : MonoBehaviour {
 		transform.localScale = new Vector3(
 			worldScreenWidth / sr.sprite.bounds.size.x,
 			worldScreenHeight / sr.sprite.bounds.size.y, 1);
+	}
+
+	//Function for fading to black.  Also handles changing the sprite.  If the current image was the last image, it goes to the next Scene after fading to black.
+	//Works by making the image transparent, exposing the black background.
+	void FadetoBlack(){
+		GetComponent<SpriteRenderer>().color = Color.Lerp(GetComponent<SpriteRenderer>().color, Color.clear, fadeSpeed * Time.deltaTime);
+		//once close enough to transparent, go fully transparent and undergo logic to display next image/go to next Scene
+		if(GetComponent<SpriteRenderer>().color.a <= 0.05f){
+			GetComponent<SpriteRenderer>().color = Color.clear;
+			doFade = false;
+			currentImage++;
+			if(currentImage < cutsceneSprites.Length){
+				GetComponent<SpriteRenderer>().sprite = cutsceneSprites[currentImage];
+				nextFrameTime += cutsceneTime[currentImage];
+			}
+			else{
+				Application.LoadLevel(nextScene.name);
+			}
+			doUnfade = true;
+		}
+	}
+	//Function for fading to image
+	void FadetoImage(){
+		GetComponent<SpriteRenderer>().color = Color.Lerp(GetComponent<SpriteRenderer>().color, Color.white, fadeSpeed * Time.deltaTime);
+		//Once close enough to fully visable, go fully visable and unset doUnfade to allow counter to run
+		if(GetComponent<SpriteRenderer>().color.a >= 0.95f){
+			GetComponent<SpriteRenderer>().color = Color.white;
+			doUnfade = false;
+		}
 	}
 
 	/*old gui code
