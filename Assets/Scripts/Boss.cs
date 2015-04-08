795c7f46f9836eb6046d81e6e46f126629194a32
Darkchicken <joshshucker@hotmail.com>
2015-04-08 12:47:53 -0700

    Boss has spawn/firing animations

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -2,9 +2,18 @@
 using System.Collections;
 
 public class Boss : GenericCharacter {
+	private Animator animator;
 	Vector3 playerPosition, diff;
+	private Vector2 sPosition;
 	float rotation;
 	GameObject boss;
+
+	// Use this for initialization
+	void Start () 
+	{
+		animator = this.GetComponent<Animator>();
+		InvokeRepeating ("SpawnBoss",4, Random.Range (4,6));	
+	}
 
 	// Update is called once per frame
 	void Update () 
@@ -12,12 +21,21 @@ public class Boss : GenericCharacter {
 		RotateToPlayer ();
 		theta = new Vector3(0, 0, rotation);//z value controls rotation, 0 is facing to the right
 		currentTime += Time.deltaTime;
-		
-		if (currentTime >= fireRate) 
+		if (!this.animator.GetCurrentAnimatorStateInfo(0).IsName("Spawning"))
 		{
-			fireArrow("EnemyArrow");
-			currentTime = 0;            
+			if (currentTime >= fireRate)
+			{
+				//animator.SetBool("Firing", true);
+				fireArrow("EnemyArrow");
+				currentTime = 0;
+				animator.SetBool("Firing", true);
+			}
+			else
+			{
+				animator.SetBool("Firing", false);
+			}
 		}
+
 		if (health <= 0) 
 		{
 			Destroy (gameObject);
@@ -49,6 +67,23 @@ public class Boss : GenericCharacter {
 			health--;
 			RePool(col.gameObject);
 		}
+	}
+	void SpawnBoss () 
+	{		
+		sPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(200,Screen.width-200), 
+		                                                       Random.Range(200,Screen.height-200), 
+		                                                       Camera.main.farClipPlane/2));
+		//Get he size of a collider at a position
+		Collider2D[] hitColliders = Physics2D.OverlapCircleAll(sPosition,
+		                                                       Mathf.Abs(collider2D.renderer.bounds.size.x - collider2D.renderer.bounds.size.x) + 2);
+		
+		
+		if (hitColliders.Length == 0) {
+			transform.position = sPosition;
+			
+		}
+		
+		
 	}
 	
 	
