d77a5b18952915552a5cf17247ec7ee8966b3ff2
Joel Lee <lee.joel395@gmail.com>
2015-04-12 21:09:51 -0700

    Loading Screen added
    Loading screen will now show up between the previous level and the next
    cutscene. Despite the short load times, it is there.
    
    Changed the StartMenu to load the loading screen instead.
    Changed the StageSelect to load their respective loadscreens.

---
+++ Assets/Scripts/LoadScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScreen : MonoBehaviour {
+	// Use this for initialization
+	public int level;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		if(Application.GetStreamProgressForLevel("Level"+level+"-Village") ==1){ //if we have loaded the level
+			Application.LoadLevel("Level"+level+"Cutscene");					 //load this scene
+		}
+	}
+}
