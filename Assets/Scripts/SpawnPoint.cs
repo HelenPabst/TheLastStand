8eeeb09e52eb7314f9d2e412289b92929037aab2
Steven McCracken <stevenamccracken@gmail.com>
2015-04-20 19:42:14 -0700

    Revert "Revert "Spawn point customizations""
    This reverts commit 3255a7dfad68a4a281de94771f1fde6ae4b4657b.

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -5,37 +5,39 @@ using System.Collections;
 public class SpawnPoint : MonoBehaviour {
 	//We can make this is a list later if multiple types of ememies need to spawn from the same point.
 	public GameObject spawnEnemy;
-	public float delay;
+	public float initialSpawnDelay;
+	public float mainSpawnDelay;
+	public int totalSpawns;
 	private bool EnemyCheck = false;
 	private float Timer;
 	private GameObject spawnedObject;
-	//Spawn Position
-	private Vector2 sPosition;
+	private Vector2 sPosition; // Spawn position
 	void  Start (){
 		//makes spawnpoints invisible during gameplay
 		renderer.enabled = false;
-		Timer = Time.time + delay;
+		Timer = Time.time + mainSpawnDelay;
 		sPosition = new Vector2(transform.position.x, transform.position.y);
-		//Runs SpawnEnemy 1 second and repeats every Delay seconds
-		InvokeRepeating ("SpawnEnemy", 1, delay);
+		//Runs SpawnEnemy after initialSpawnDelay seconds and repeats every mainSpawnDelay seconds
+		InvokeRepeating ("SpawnEnemy", initialSpawnDelay, mainSpawnDelay);
 	}
 	
 	void  SpawnEnemy() {
-		Collider2D[] hitCollidersEnemy = Physics2D.OverlapCircleAll(sPosition, 1);
+		Collider2D[] hitCollidersEnemy = Physics2D.OverlapCircleAll (sPosition, 1);
 		if (Timer < Time.time && !EnemyCheck) { // check if real time has caught up with timer
-			spawnedObject = ObjectPool.instance.GetObjectForType(spawnEnemy.name, true);
-			if(spawnedObject == null) {
-				Timer += 2;
-			} 
-			else {
-				spawnedObject.transform.position = transform.position;
-				spawnedObject.transform.rotation = transform.rotation;
-				EnemyCheck = true;
-				sPosition = new Vector2(spawnedObject.transform.position.x, spawnedObject.
+			if(totalSpawns != 0) { // Section of code will execute totalSpawns number of times. If totalSpawns is -1, then it will always execute
+				spawnedObject = ObjectPool.instance.GetObjectForType (spawnEnemy.name, true); // Spawns enemy in game
+				if (spawnedObject == null) {
+					Timer += 2;
+				} else {
+					spawnedObject.transform.position = transform.position;
+					spawnedObject.transform.rotation = transform.rotation;
+					EnemyCheck = true;
+					sPosition = new Vector2 (spawnedObject.transform.position.x, spawnedObject.
 				                        transform.position.y);
+				}
+				totalSpawns--;
 			}
-		}
-		else if (hitCollidersEnemy.Length == 0) {
+		} else if (hitCollidersEnemy.Length == 0) {
 			EnemyCheck = false;
 		}
 	}
