8411f138b6d7f061c789f85e42a653b74a133782
Coder692 <beep690@gmail.com>
2015-04-21 22:38:27 -0700

    Button to skip to level
    Just a simple GUI button on the bottom right of each level cutscene that
    goes directly to the subsequent level on press

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+	
+	public void Change_Scene (int SceneToGoTo) {
+		Application.LoadLevel(Application.loadedLevel+1);
+	}
+
+}
