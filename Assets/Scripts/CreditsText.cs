5748ff32f30f78d121d50938707b2d108a4f52a1
Coder692 <beep690@gmail.com>
2015-04-27 20:10:19 -0700

    Credits
    A very simple credits scene where the speed can be controlled in the
    inspector.  Only contains credits for the art and technical teams so
    far.

---
+++ Assets/Scripts/CreditsText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsText : MonoBehaviour {
+	
+	public GameObject camera;
+	public int speed = -18;
+	public string level;
+	
+	
+	void Update () {
+		
+		this.transform.Translate (Vector3.down * Time.deltaTime * speed);
+		
+	}
+	
+	IEnumerator waitFor(){
+		yield return new WaitForSeconds (20);
+		Application.LoadLevel (level);
+		
+	}
+	
+	
+}
