71f6e6599cdc1f07c2977f73f1900df3f0849ce7
Darkchicken <joshshucker@hotmail.com>
2015-04-28 02:42:15 -0700

    Credits again

--- Assets/Scripts/CreditsText.cs
+++ Assets/Scripts/CreditsText.cs
@@ -6,17 +6,26 @@ public class CreditsText : MonoBehaviour {
 
 	public int speed = -18;
 	public string level;
+	public float runTime;
 	
-	
+	void Start()
+	{
+		InvokeRepeating("creditTime", 0, 1);
+	}
 	void Update () {
 		
 		this.transform.Translate (Vector3.up * Time.deltaTime * speed);
+		if (runTime <= 0) 
+		{
+			Application.LoadLevel (level);
+		}
 		
 	}
 	
-	IEnumerator waitFor(){
-		yield return new WaitForSeconds (20);
-		Application.LoadLevel (level);
+	void creditTime()
+	{
+		runTime -= 1;
+
 		
 	}
 	
