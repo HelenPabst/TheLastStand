beb6394a63223da568946b5a9e6b6bd8d12ef4d3
Darkchicken <joshshucker@hotmail.com>
2015-05-07 18:42:25 -0700

    Music in ending cutscene
    Added lvl 1 music as temporary ending cutscene music
    Working on fading it at end

--- Assets/Scripts/TempEnding.cs
+++ Assets/Scripts/TempEnding.cs
@@ -8,10 +8,14 @@ public class TempEnding : MonoBehaviour {
 	public string level;
 	public float runTime;
 	public float scrollEnd;
+	public AudioSource endMusic;
+	public float audio1Volume  = 1.0f;
 	// Use this for initialization
 	void Start () 
 	{
+		Invoke("fadeOut", 35.0f);
 		InvokeRepeating("cutsceneTime", 0, 1);
+
 	}
 	
 	// Update is called once per frame
@@ -31,5 +35,12 @@ public class TempEnding : MonoBehaviour {
 	void cutsceneTime()
 	{
 		runTime -= 1;
+	}
+	void fadeOut() {
+		while(audio1Volume > 0.1)
+		{
+			audio1Volume -= 0.1f * Time.deltaTime;
+			endMusic.volume = audio1Volume;
+		}
 	}
 }
