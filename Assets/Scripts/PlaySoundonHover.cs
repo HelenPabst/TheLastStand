4f625b968fef04d38127a6720554b05b6d954a9a
Stanley Ung <stanleyung167@gmail.com>
2018-05-02 18:47:06 -0700

    sound on hover

---
+++ Assets/Scripts/PlaySoundonHover.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundonHover : MonoBehaviour
+{ 
+    private void OnMouseOver()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+}
